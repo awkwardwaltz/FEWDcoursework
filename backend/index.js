f671c9acba2bca47663329d26a50bc2fd0fe5f0e
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2022-12-18 22:03:33 +0000

    route change

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,10 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false }));
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
-//router.get("/", controller.listRecipe);
+router.get("/", controller.listRecipe);
 router.get("/recipes", controller.listRecipe);
 router.post("/update", controller.updateRating)
 router.get("/reset", controller.newList);
-
-
 
 router.get('*', (req,res) => {
   res.sendFile(path.join(__dirname+'/frontend/build/index.html'))
