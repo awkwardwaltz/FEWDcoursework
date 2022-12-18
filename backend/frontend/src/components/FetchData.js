34c87ee2f723b03a8d54667ecfcdc85b57b7cc88
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2022-12-18 20:46:56 +0000

    proxy

--- backend/frontend/src/components/FetchData.js
+++ backend/frontend/src/components/FetchData.js
@@ -33,7 +33,7 @@ const FetchData = () => {
     }]);
 
     const fetchData = useCallback(() => {
-        const url = "/recipes"
+        const url = "/"
         fetch(url)
             .then((response) => response.json())
             .then((incomingData) => {
