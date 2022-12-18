e6718a433ad833135e913a2907e4db403dc3e7d8
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2022-12-18 21:28:29 +0000

    route change

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path")
 const app = express();
-
 const cors = require('cors');
+const bodyParser  = require('body-parser');
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
-const bodyParser  = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false }));
 
@@ -17,7 +17,7 @@ app.get('*', (req,res) => {
 
 const router = require('./routes/routes');
 app.use('/', router);
-
-app.listen(process.env.PORT || 5000, () => {
+       const portNumber = (process.env.PORT || 5000)
+app.listen(portNumber, () => {
   console.log("Server started on port 5000. Ctrl^c to quit.");
 });
