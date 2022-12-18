aac3dc5e18908da467d275f01496f6ae0027b2b1
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2022-12-18 22:09:54 +0000

    route change

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const path = require("path")
 const app = express();
 const cors = require('cors');
 const bodyParser  = require('body-parser');
-const controller = require('controllers/controllers')
+const controller = require('./controllers/controllers')
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false }));
